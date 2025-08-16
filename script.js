60730048e6b991652e0c2151dd02c7bdd50a922a
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-08-16 23:58:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//your JS code here. If required.
+// //your JS code here. If required.
 for (let i=1; i<=20; i++) {
 	if (i % 3 == 0 && i % 5 == 0) {
 		alert("FizzBuzz");
-	} else if (i & 3 == 0) {
+	} else if (i && 3 == 0) {
 		alert("Fizz");
-	} else if (i & 5 == 0) {
+	} else if (i && 5 == 0) {
 		alert("Buzz");
 	} else {
 		alert(i);
 	}
 }
 
-return output;
+alert(output);
